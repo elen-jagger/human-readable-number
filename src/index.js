0b7c89851209f385b2bcf2d5525507afe56d6ad1
Elena Leusik <elena.leusik@gmail.com>
2021-08-10 15:01:42 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function toReadable (number) {
+    let list = {
+        0 : 'zero' ,
+        1 : 'one' , 
+        2 : 'two' ,
+        3 : 'three' ,
+        4 : 'four' ,
+        5 : 'five' ,
+        6 : 'six' ,
+        7 : 'seven' ,
+        8 : 'eight' ,
+        9 : 'nine' ,
+        10 : 'ten' ,
+        11 : 'eleven' ,
+        12 : 'twelve' ,
+        13 : 'thirteen' ,
+        14 : 'fourteen' ,
+        15 : 'fifteen' ,
+        16 : 'sixteen' ,
+        17 : 'seventeen' ,
+        18 : 'eighteen' ,
+        19 : 'nineteen' ,
+        20 : 'twenty' ,
+        30 : 'thirty' ,
+        40 : 'forty' ,
+        50 : 'fifty' ,
+        60 : 'sixty' ,
+        70 : 'seventy' ,
+        80 : 'eighty' ,
+        90 : 'ninety' ,
+    } ;   
+
+    if ((number >= 0) && (number <= 20)) { return list[number] ; }
+    else if ((number > 20) && (number < 100)) {
+        let decade = (Math.floor(number / 10)) * 10 ;
+        let unit = number % 10 ;
+
+        if (unit == 0) { return list[decade] ; }
+        else { return list[decade] + ' ' + list[unit] ; }
+    }
+    else {
+        let hundreed = Math.floor(number / 100) ;
+        let decade = (Math.floor((number % 100) / 10)) * 10 ;
+        let unit = (number % 100) % 10 ;
+
+        if (unit == 0) { 
+            return list[hundreed] + ' hundreed ' + list[decade] ; 
+        }
+        else {
+            return list[hundreed] + ' hundreed ' + list[decade] + ' ' + list[unit] ;
+        }
+        
+
+    }
   
 }
